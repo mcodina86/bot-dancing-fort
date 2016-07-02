02076b9915986d3c8c0f343ddc78d8f029534a3d
mcodina86 <matcod86@gmail.com>
2016-07-02 12:31:26 -0300

    Updated config example

--- config.example.py
+++ config.example.py
@@ -1,3 +1,12 @@
 def getKey():
     key = "SECRET API KEY"
     return key
+
+def getTwitterKeys():
+    cfg = {
+    "consumer_key"        : "",
+    "consumer_secret"     : "",
+    "access_token"        : "",
+    "access_token_secret" : ""
+    }
+    return cfg
